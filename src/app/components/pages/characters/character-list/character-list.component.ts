e0f8a5ccf484e7edcf02fe2f846971b8ac5407f3
Fabio Andres Roa Garcia <froa@tramiti.co>
2022-11-03 00:18:32 -0500

    avance get02

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,56 +1,66 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, ParamMap, Router } from '@angular/router';
 import { Character } from '@app/shared/interface/character.interface';
 import { CharacterService } from '@app/shared/services/character.service';
-type RequestInfo = {Next: String};
-import { take } from 'rxjs';
+type RequestInfo = { Next: String };
+import { filter, map, take } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  styleUrls: ['./character-list.component.scss'],
 })
 export class CharacterListComponent implements OnInit {
-  characters:Character[]=[];
-  info: RequestInfo={ Next:''};
+  characters: Character[] = [];
+  info: RequestInfo = { Next: '' };
 
   private pageNum = 1;
-  private query : string ='' ;
-  private hideScrollHeight =200;
-  private showScrollHeight =500;
+  private query: string = '';
+  private hideScrollHeight = 200;
+  private showScrollHeight = 500;
   constructor(
     private characterSvc: CharacterService,
-    private route: ActivatedRoute
-    ) { }
-
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.onUrlChanged();
+  }
 
   ngOnInit(): void {
-    this.getDataFromService();
+    this.getCharactersByQuery();
   }
 
-  private getCharactersByQuery():void{
-    // this.route.queryParams.pipe(
-    //   take(1) ).subscribe((params:ParamMap)=>{
-    //     this.query=params ['q'];
-    //     this,this.getDataFromService()
-    //   })
-  }
+private onUrlChanged(): void{
+this.router.events
+.pipe(filter((event) => event instanceof NavigationEnd))
+.subscribe(() => {
+  this.characters =[];
+  this.pageNum = 1
+  this.getCharactersByQuery();
+});
 
-
-private getDataFromService():void{
-this.characterSvc.searchCharacters(this.query, this.pageNum)
-.pipe(  take(1))
-  .subscribe( (res: any)=> {
-    if(res?.results ?.length>0){
-      const {info, results}= res;
-      this.characters=[...this.characters, ...results];
-      this.info=info;
-    }
-    else{
-      this.characters=[];
-    }
-
-  })
 }
+  private getCharactersByQuery() {
+       this.route.queryParams.pipe(take(1)).subscribe((params) => {
+      this.query = params['q'];
+      this.getDataFromService();
+    });
+  }
+
+  private getDataFromService(): void {
+    this.characterSvc
+      .searchCharacters(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        if (res?.results?.length > 0) {
+          const { info, results } = res;
+          this.characters = [...this.characters, ...results];
+          this.info = info;
+        } else {
+          this.characters = [];
+        }
+      });
+  }
 }
